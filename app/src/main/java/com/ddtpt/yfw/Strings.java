49243c3ac25bfb060e93563069e8dc74cbbe27ca
Nathan <nathan921@gmail.com>
2016-07-11 18:25:25 -0700

    String
    Commiting String

---
+++ app/src/main/java/com/ddtpt/yfw/Strings.java
@@ -0,0 +1,16 @@
+package com.ddtpt.yfw;
+
+/**
+ * Created by Nathan on 7/7/2016.
+ */
+
+public class Strings {
+    public static final String SECRET = "oauth_secret";
+    public static final String TOKEN = "oauth_token";
+    public static final String OAUTH_SIGNATURE_METHOD = "oauth_signature_method";
+    public static final String SIGNATURE_METHOD = "HMAC-SHA1";
+    public static final String OAUTH_TIMESTAMP = "oauth_timestamp";
+    public static final String OAUTH_NONCE = "oauth_nonce";
+    public static final String OAUTH_SIGNATURE = "oauth_signature";
+    public static final String OAUTH_VERSION = "oauth_version";
+}
